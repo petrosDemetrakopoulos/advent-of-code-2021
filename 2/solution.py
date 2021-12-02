d3b9558af7b5b4d5d5720672cedf77fbc8c1264d
Petros <psdeme@hotmail.com>
2021-12-02 18:06:49 +0200

    second puzzle

---
+++ 2/solution.py
@@ -0,0 +1,14 @@
+with open('input.txt') as f:
+    lines = map(lambda x: [x.replace("\n","").split(" ")[0], int(x.replace("\n","").split(" ")[1])], f.readlines())
+
+depth = 0
+horizontal = 0
+
+for move in lines:
+    if move[0] == 'forward':
+        horizontal += move[1]
+    elif move[0] == 'down':
+        depth += move[1]
+    elif move[0] == 'up':
+        depth -= move[1]
+print("horizontal: " + str(horizontal) + " depth: " + str(depth))
